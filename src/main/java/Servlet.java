ba9e690e7e5ba639d227f239f22fcc576450e2b3
Michael Satterfield <michael.f.satterfield@gmail.com>
2020-11-02 16:46:07 -0600

    servlets tomcat exercises

--- src/main/java/Servlet.java
+++ src/main/java/Servlet.java
@@ -4,11 +4,16 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
-@WebServlet(name = "Servlet")
+@WebServlet(name = "Servlet", urlPatterns = "/users")
 public class Servlet extends HttpServlet {
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
 
+        String name = request.getParameter("name");
+        out.println("<h3>Hello "+ name + "<h3>");
     }
 }
