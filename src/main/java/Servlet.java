46cef344fc60febf47e5909d0adcd0259238c4df
Michael Satterfield <michael.f.satterfield@gmail.com>
2020-11-03 10:04:26 -0600

    added html and post and counter

--- src/main/java/Servlet.java
+++ src/main/java/Servlet.java
@@ -9,11 +9,26 @@ import java.io.PrintWriter;
 @WebServlet(name = "Servlet", urlPatterns = "/users")
 public class Servlet extends HttpServlet {
 
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-    response.setContentType("text/html");
+//    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//    response.setContentType("text/html");
+//        PrintWriter out = response.getWriter();
+//
+//        String name = request.getParameter("name");
+//        out.println("<h3>Hello "+ name + "</h3>");
+//    }
+//    when a user posts a input
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws  IOException {
+        response.setContentType("text/html");
         PrintWriter out = response.getWriter();
 
         String name = request.getParameter("name");
-        out.println("<h3>Hello "+ name + "<h3>");
+        out.println("<h3>Hello "+ name + "</h3>");
     }
-}
+    private int counter = 0;
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        counter += 1;
+        response.getWriter().println("<h1>The count is " + counter + ".</h1>");
+
+
+}}
