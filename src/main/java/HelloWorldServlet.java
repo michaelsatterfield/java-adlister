378841b9a2d21d2340df174f332277d58bf3db73
Michael Satterfield <michael.f.satterfield@gmail.com>
2020-11-03 09:34:08 -0600

    added hello %s

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -6,14 +6,16 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 
-@WebServlet(name="HelloWorldServlet", urlPatterns = "/hello-world")
+@WebServlet(name="HelloWorldServlet", urlPatterns = "/hello")
 //*******servlet method for tomcat*******
+
+
 public class HelloWorldServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
         res.setContentType("text/html");
         PrintWriter out = res.getWriter();
         out.println("<h1>Hello, World!</h1>");
-        out.printf("<h1>Hello,%s,Michael</h1>");
+        out.printf("<h1>Hello %s</h1>","Michael");
 
     }
 }
